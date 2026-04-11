3ba8049131da0ecf81a5dfcc36b2d69c8e8714c5
nguyenngocdue <duengocnguyen@gmail.com>
2026-04-11 10:15:48 +0700

    DevToolV2Commands -> sayhellofeat: update code

---
+++ DevToolV3Commands/Commands/TestSayHelloCommand.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace DevToolV3Commands
+{
+    [Transaction(TransactionMode.Manual)]
+    public class TestSayHelloCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Hello", "Hello, Revit!");
+            return Result.Succeeded;
+        }
+    }
+}
